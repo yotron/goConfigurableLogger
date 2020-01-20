3385cf77e847ee9ced4920f8203ac41b6d0a1d4d
joernkleinbub <joern.kleinbub@yotron.de>
2020-01-20 10:52:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+#  [![yotron](logo-yotron.png)](http://www.yotron.de)
+
+[YOTRON](http://www.yotron.de) is a consultancy company which is focused on DevOps, Cloudmanagement and 
+Data Management with NOSQL and SQL-Databases. Visit us on [ www.yotron.de ](http://www.yotron.de)
+
+# goConfigurableLogger
+This project is a small and simple logging tool for Go projects which is configurable via a configuration File.
+
+With this project you can Setup a logger which is configurable centrally with a log file in your project.
+The file is located in your root folder of your go project. You are able to manage
+
+- the goal to the logs
+- the level of the log
+
+It is possible to write into a file, give it out in the standard output or discard the logs.
+
+## Example Configuration file
+An example logfile is looking like that:
+```
+---
+log_level: info
+log_writer: file
+log_file: metrics_exporter.log
+```
+
+## Setup of the configuration file
+The configuration file is a file in the root folder of your go-project. It is called `conf.logging.yml`.
+The setup of the logs shall be done in that way.
+
+`log_level`: The level of the logging. Possible values are `debug`, `info`, `warn`, `error`
+`log_writer`: The goal of the the logging. Possible values are `file`, `stdout`, `discard`
+`log_file`: If you write to a file, you have to setup the logfile. If you only want to write to a logfile in you root
+folder of the project, you do not need a folder path for the file.
+
+## How to use the debugger
+You can import the logger in your package in that way:
+
+. "### TBD ##"
+
+Th following log level are provided. 
+
+- `info`
+- `warn`
+- `error`
+- `debug`
+
+As usual the level are hierarchally organized. 
+This means for example, in with the log level `debug` all other messages will be handled as well. 
+
+All log level inherits from the ordinary log package of Golang (https://golang.org/pkg/log). 
+All string formatting functions like `Println`, `Printf` ... are supported please have a look at (https://golang.org/pkg/log/#Logger)
+
+Here are some example how to use the logger within your project:
+
+`Info.Println("That is my message.")`
+
+`Warn.Println(msg)`
+
+`Error.Println("File malformed:", path, "Error:", err)`
+
+`Debug.Printf("Debug: %message - %error", mss, err)`
---
+++ yotronlogger.go
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package yotronlogger
+
+import (
+	"gopkg.in/yaml.v2"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type ConfLog struct {
+	Log_level  string `yaml:"log_level"`
+	Log_writer string `yaml:"log_writer"`
+	Log_file   string `yaml:"log_file"`
+}
+
+type writer struct {
+	Debug io.Writer
+	Warn  io.Writer
+	Info  io.Writer
+	Error io.Writer
+}
+
+var (
+	Debug *log.Logger
+	Warn  *log.Logger
+	Info  *log.Logger
+	Error *log.Logger
+
+	conf      ConfLog
+	logWriter writer
+)
+
+func init() {
+	conf.readMainConfig()
+	setWriter()
+	handleLogger()
+}
+
+func setWriter() {
+	logWriter = writer{ioutil.Discard, ioutil.Discard, ioutil.Discard, ioutil.Discard}
+	if conf.Log_writer != "discard" {
+		lw := getLogWriter()
+		switch conf.Log_level {
+		case "error":
+			logWriter = writer{ioutil.Discard, ioutil.Discard, ioutil.Discard, lw}
+		case "warn":
+			logWriter = writer{ioutil.Discard, ioutil.Discard, lw, lw}
+		case "info":
+			logWriter = writer{ioutil.Discard, lw, lw, lw}
+		case "debug":
+			logWriter = writer{lw, lw, lw, lw}
+		default:
+			panic("Failed to set LogLevel. Value wrong:" + conf.Log_level)
+		}
+	}
+}
+
+func getLogFile() io.Writer {
+	logFile, err := os.OpenFile(conf.Log_file, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		panic("Could not Create or Write to file: " + conf.Log_file)
+	}
+	return logFile
+}
+
+func getLogWriter() io.Writer {
+	switch conf.Log_writer {
+	case "file":
+		return getLogFile()
+	case "stdout":
+		return os.Stdout
+	case "discard":
+		return ioutil.Discard
+	default:
+		panic("Failed to set LogWriter. Value wrong:" + conf.Log_level)
+	}
+}
+
+func handleLogger() {
+	Debug = log.New(logWriter.Debug,
+		"DEBUG: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(logWriter.Error,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Warn = log.New(logWriter.Warn,
+		"WARN: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(logWriter.Info,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func (conf *ConfLog) readMainConfig() {
+	err := yaml.Unmarshal(readYAMLFile("conf.logging.yml", conf), &conf)
+	if err != nil {
+		panic("Error during unmarshalling logfile")
+	}
+}
+
+func readYAMLFile(path string, type_struct interface{}) []byte {
+	err := yaml.Unmarshal(readFile(path), &type_struct)
+	if err != nil {
+		panic("Logfile malformed")
+	}
+	byte, err := yaml.Marshal(type_struct)
+	return byte
+}
+
+func readFile(path string) []byte {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic("Could not read file: " + path)
+	}
+	return file
+}
